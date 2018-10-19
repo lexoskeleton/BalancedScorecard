157ed6c248ba585a24112bff52d4e24008bca222
Vitalii <avalor1991@gmail.com>
2018-10-19 01:16:57 -0400

    pdf tab

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
 export default {
-  loginSubmit: function (loginData) {
-
+  loginSubmit: function(loginData) {
     return axios.post("/api/auth/login", loginData);
   },
   //registers a new user into the database.
-  registerSubmit: function (registerData) {
+  registerSubmit: function(registerData) {
     return axios.post("/api/auth/register", registerData);
   },
   //registers a users contact message into the database.
-  contactSubmit: function (contactData) {
+  contactSubmit: function(contactData) {
     return axios.post("/api/contact", contactData);
   },
 
@@ -33,21 +32,4 @@ export default {
   deleteTask: function(id) {
     return axios.delete("/api/task/" + id);
   }
-};
-
-  getData: function (profitData) {
-    return axios.get("/api/bscd", profitData)
-  },
-
-  getAllTasks: function () {
-    return axios.get("/api/task");
-  },
-
-  postTask: function (task) {
-    return axios.post("/api/task");
-  },
-
-  deleteTask: function (id) {
-    return axios.delete("/api/task/" + id);
-  },
 };
